4559baa0f77690c3be35c910252fc567a7c7ef45
Qianhao96 <qianhao.liu@gmail.com>
2020-05-30 18:19:31 +0800

    import Radium from radium; import styled from styled-components; import classes from ./Person.module.css; these three way of adding css only scop to a specific componenet

--- react-prac/src/Person/Person.js
+++ react-prac/src/Person/Person.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import Radium from 'radium';
-import styled from 'styled-components';
+//import styled from 'styled-components'
+
+import classes from './Person.module.css'
 
 const person = (props) => {
     // const style = {
@@ -9,22 +11,22 @@ const person = (props) => {
     //     }
     // };
 
-    const StyledDiv = styled.div`
-        @media (max-width: 500px){
-            width: 100%;
-            background-color: red;
-        }
-        background-color: blue;
-        width: 60%;
-    `;
+    // const StyledDiv = styled.div`
+    //     @media (min-width: 1000px) {
+    //         width:'700px';
+    //         background-color: red;
+    //     }
+    // `;
 
     return (
-        <StyledDiv>
-            <p onClick={props.click}>I'm {props.name}</p>
-            {props.children}
-            <br/>
-            {props.changed != null ? <input type="text" onChange={props.changed} value={props.name} /> : null}
-        </StyledDiv>
+    <div className={[classes.Blue, classes.Yellow, classes.Red].join(' ')}>
+        <p onClick={props.click}>I'm {props.name}</p>
+        {props.children}
+        <br/>
+        {props.changed != null ? <input  type="text" onChange={props.changed} value={props.name} /> : null}
+    </div>
+    
+    
     )
 };
 
