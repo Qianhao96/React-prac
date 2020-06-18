02b536588bdb15994833c3f59c8c4cd91d752261
Qianhao96 <qianhao.liu@gmail.com>
2020-06-18 22:09:39 +0800

    last commit for react prc

--- react-prac/src/components/Persons/Person/Person.js
+++ react-prac/src/components/Persons/Person/Person.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import Radium from 'radium';
 //import styled from 'styled-components';
+import Auxiliary from '../../../hoc/Auxiliary'
+
 
 import classes from './Person.module.css';
 
-const person = (props) => {
+const Person = (props) => {
     // const style = {
     //     '@media (min-width: 1000px)':{
     //         minWidth:'700px'
@@ -19,16 +21,42 @@ const person = (props) => {
     // `;
 
     return (
-    <div className={[classes.Blue, classes.Yellow, classes.Red].join(' ')}>
+    <Auxiliary classes={[classes.Blue, classes.Yellow, classes.Red].join(' ')}>
         <p onClick={props.click}>I'm {props.name}</p>
         {props.children}
         <br/>
         {props.changed != null ? <input  type="text" onChange={props.changed} value={props.name} /> : null}
-    </div>
+    </Auxiliary>
     
     
     )
 };
 
 // export default Radium(person);
-export default person;
+export default Person;
+
+
+// class Person extends Component {
+//     // const style = {
+//     //     '@media (min-width: 1000px)':{
+//     //         minWidth:'700px'
+//     //     }
+//     // };
+
+//     // const StyledDiv = styled.div`
+//     //     @media (min-width: 1000px) {
+//     //         width:'700px';
+//     //         background-color: red;
+//     //     }
+//     // `;
+//     render (){
+//         return[
+//                 <p key="i1" onClick={this.props.click}>I'm {this.props.name}</p>,
+//                 <p  key="i2">{this.props.children}</p>,
+//                 <p  key="i3">{this.props.changed != null ? <input  type="text" onChange={this.props.changed} value={this.props.name} /> : null}</p>
+//             ];
+//     }
+// };
+
+// // export default Radium(person);
+// export default Person;
---
+++ react-prac/src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Auxiliary = props => <div className={props.classes}>{props.children}</div>;
+
+export default Auxiliary;
